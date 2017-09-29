ad5c69ad7bfa501defb5a209b7b09c9dee7c2f22
Long Nguyen <long.nguyen@coloradocollege.edu>
2017-09-28 22:37:40 -0600

    failure modes

--- theory/failure_modes/hw_race.c
+++ theory/failure_modes/hw_race.c
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
         t_hello[i]=-1;
     }
     i=0;
-    #pragma omp parallel num_threads(100)
+    #pragma omp parallel num_threads(8)
     {
         t_hello[i]=omp_get_thread_num();
         printf("Hello from %d\n",t_hello[i]);
