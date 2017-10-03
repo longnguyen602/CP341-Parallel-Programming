6498dfab3e9e812ca8940fc46e8948026f1d5664
Long Nguyen <long.nguyen@coloradocollege.edu>
2017-10-03 13:09:16 -0600

    traffic

--- theory/architecture/traffic.c
+++ theory/architecture/traffic.c
@@ -10,34 +10,43 @@ struct _LinkedRecord{
 typedef struct _LinkedRecord LinkedRecord_t;
 
 void LinkedRecord_init(LinkedRecord_t* LR){
-  LR = malloc(sizeof(LinkedRecord_t));
   LR->year = 1;
 }
 
 int parse_line(char* line){
   int year;
+  int e;
   //printf("%s", line);
   char str_year[5] = {line[6], line[7], line[8], line[9], '\0'};
-  printf("%s\n", str_year);
+  printf("%s Should be String\n", str_year);
   printf("TEST\n");
   year = atoi(str_year);
-  printf("%d", year);
+  if (year==0){
+    year=-1;
+  }
+  printf("%d Should be INT\n", year);
   return year;
 }
 
 LinkedRecord_t * find_record(LinkedRecord_t * head, int year) {
+    printf("Inside find_record %d\n", year);
     LinkedRecord_t * current = head;
     LinkedRecord_t * result;
     while (current != NULL) {
-        //printf("%d\n", current->year);
+        printf("Inside while loop in find_record\n");
+        //printf("%d current year\n", current->year);
         if (current->year == 1){
+          printf("This  should be true\n");
           current->year = year;
+          printf("%d current year after true\n", current->year);
         }
         if (current->year == year){
+          printf("Inside next\n");
           result = current;
         }
         current = current->next;
     }
+    printf("Returning result after this\n");
     return result;
 }
 
@@ -55,8 +64,10 @@ int main(int argc, char** argv) {
     printf("Usage: %s <traffic data>\nExiting...\n",argv[0]);
   }
   int year;
-  LinkedRecord_t LR;
-  LinkedRecord_init(&LR);
+  LinkedRecord_t *LR;
+  LR = malloc(sizeof(LinkedRecord_t));
+  LinkedRecord_init(LR);
+  printf("%d After init\n",LR->year);
   FILE* f; // file handle to use for read and write
   int e;
   //The following 10 lines of code are reference from an online source
@@ -66,14 +77,19 @@ int main(int argc, char** argv) {
     perror("fopen source-file");
     return 1;
   }
-
-  while (fgets(buf, sizeof(buf), f) != NULL){
+  int j =0;
+  //while (fgets(buf, sizeof(buf), f) != NULL){
+  while (j<4){
+    fgets(buf, sizeof(buf), f);
+    printf("%s\n", buf);
     LinkedRecord_t* record;
     buf[strlen(buf) - 1] = '\0'; // eat the newline fgets() stores
     year=parse_line(buf);
-    printf("%d", year);
-    record = find_record(&LR, year);
+    //printf("%d", year);
+    printf("%d Before call find_record\n",LR->year);
+    record = find_record(LR, year);
     record->stops+=1;
+    j+=1;
     //printf("%s\n", buf);
   }
   fclose(f);
