943977c5e00cff15a187f0596c4e607bb5cecbb6
Long Nguyen <long.nguyen@coloradocollege.edu>
2017-10-03 23:26:57 -0600

    openmp.c

--- patterns/map/map/serial.c
+++ patterns/map/map/serial.c
@@ -2,13 +2,13 @@
  * This program takes in a hash and attempts to find a matching 8 digit pin
  */
 
-// sprintf, printf 
+// sprintf, printf
 #include <stdio.h>
 
 // strlen, strncmp
 #include <string.h>
 
-// exit value 
+// exit value
 #include <stdlib.h>
 
 // md5 hash function
@@ -23,17 +23,17 @@ const char* chars="0123456789";
 // tests if a hash matches a candidate password
 int test(const char* passhash, const char* passcandidate) {
     unsigned char digest[MD5_DIGEST_LENGTH]; // hash storage
-    
+
     // Compute the hash
     MD5((unsigned char*)passcandidate, strlen(passcandidate), digest);
-    
+
     // Convert the hash to a base 16 string
     char mdString[34];
     mdString[33]='\0';
     for(int i=0; i<16; i++) {
         sprintf(&mdString[i*2], "%02x", (unsigned char)digest[i]);
     }
-    
+
     // return the value of the comparison
     return strncmp(passhash, mdString, strlen(passhash));
 }
@@ -42,10 +42,10 @@ int test(const char* passhash, const char* passcandidate) {
 void genpass(long passnum, char* passbuff) {
     passbuff[8]='\0'; // enforce null termination
     int charidx; // character within array of valid characters
-    
+
     // determine the "base" for the pin string
     int symcount=strlen(chars);
-    
+
     // perform the conversion
     for(int i=7; i>=0; i--) {
         charidx=passnum%symcount;
@@ -59,7 +59,7 @@ int main(int argc, char** argv) {
         printf("Usage: %s <password hash>\n",argv[0]);
         return 1;
     }
-    
+
     char passmatch[9]; // buffer for the matched password
     long currpass=0; // current password under consideration
 
@@ -67,7 +67,7 @@ int main(int argc, char** argv) {
     struct timespec start_time;
     struct timespec end_time;
     long msec;
-    
+
     clock_gettime(CLOCK_MONOTONIC,&start_time);
     // While a match has not been found, search
     int notfound=1;
@@ -76,6 +76,7 @@ int main(int argc, char** argv) {
         genpass(currpass,passmatch);
         // check for a match
         notfound=test(argv[1], passmatch);
+        printf("%d", notfound);
         currpass++;
     }
     clock_gettime(CLOCK_MONOTONIC,&end_time);
@@ -86,4 +87,3 @@ int main(int argc, char** argv) {
     printf("found: %s in %dms\n",passmatch,msec);
     return EXIT_SUCCESS;
 }
-
