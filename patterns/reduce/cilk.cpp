2cb80680a5779328f05441ba309ed90fa0d92029
Long Nguyen <long.nguyen@coloradocollege.edu>
2017-10-09 11:09:11 -0600

    cilk.cpp

--- patterns/reduce/cilk.cpp
+++ patterns/reduce/cilk.cpp
@@ -69,8 +69,8 @@ void post_process(struct volume* v, float* cx, float* cy) {
         *wx += o->x * o->mass;
         *wy += o->y * o->mass;
     }
-    *cx = *wx/(*mass_sum);
-    *cy = *wy/(*mass_sum);
+    *cx = wx.get_value()/(mass_sum.get_value());
+    *cy = wy.get_value()/(mass_sum.get_value());
 
     return;
 }
